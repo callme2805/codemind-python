edc5d0d6624cc5265ae0daa9facda403da13d3ab
Betha Harsha vardhan <84622897+callme2805@users.noreply.github.com>
2022-10-26 20:26:31 +0530

    n=int(input()) m=list(map(int,input().split())) k=0 for i in range(0,n-1):     if m[i]>m[i+1]:         k+=0     else:         k+=1 if k==0:     print("yes") else:     print("no")

---
+++ Descending_array.py
@@ -0,0 +1,14 @@
+n=int(input())
+m=list(map(int,input().split()))
+k=0
+for i in range(0,n-1):
+    if m[i]>m[i+1]:
+        k+=0
+    else:
+        k+=1
+if k==0:
+    print("yes")
+else:
+    print("no")
+        
+    
